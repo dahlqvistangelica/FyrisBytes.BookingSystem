2cd06e353013cea6a0ccba5f2f4a40833efb99df
olofbrahm <olofbrahm@outlook.com>
2025-10-16 11:08:16 +0200

    Namespaces och using

--- BookingManager.cs
+++ BookingManager.cs
@@ -1,36 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class BookingManager
+namespace BookingSystem.BookingManager
 {
 
-    public List<Room> AllBookings { get; set; }
-    public List<Room> AllRooms { get; set; }
-    public List<string> Developers { get; set; }
 
-    public BookingManager()
+    public class BookingManager
     {
-        //Laddar data ifrån JSON filer, om filen inte finns blir det en tom lista
-        AllBookings = SaveData.LoadFromFile<Room>("allbooking.json");
-        AllRooms = SaveData.LoadFromFile<Room>("allrooms.json");
-        Developers = SaveData.LoadFromFile<string>("developers.json");
+
+        public List<Room> AllBookings { get; set; }
+        public List<Room> AllRooms { get; set; }
+        public List<string> Developers { get; set; }
+
+        public BookingManager()
+        {
+            //Laddar data ifrån JSON filer, om filen inte finns blir det en tom lista
+            AllBookings = SaveData.LoadFromFile<Room>("allbooking.json");
+            AllRooms = SaveData.LoadFromFile<Room>("allrooms.json");
+            Developers = SaveData.LoadFromFile<string>("developers.json");
+
+        }
+        public bool SaveAllData()
+        {
+            //Sparar all data till respektive fil, bool för användar val om fel händer
+            bool savedBooking = SaveData.SaveToFile(AllBookings, "allbooking.json");
+            bool savedRooms = SaveData.SaveToFile(AllRooms, "allrooms.Json");
+            bool savedDevs = SaveData.SaveToFile(Developers, "developers.json");
+            return savedBooking && savedBooking && savedDevs;
+        }
+
+        public void SortLists()
+        {
+            //PLACE HOLDER
+        }
+        public void SearchLists()
+        {
+            //PLACE HOLDER
+        }
 
     }
-    public bool SaveAllData()
-    {
-        //Sparar all data till respektive fil, bool för användar val om fel händer
-        bool savedBooking = SaveData.SaveToFile(AllBookings, "allbooking.json");
-        bool savedRooms = SaveData.SaveToFile(AllRooms, "allrooms.Json");
-        bool savedDevs = SaveData.SaveToFile(Developers, "developers.json");
-        return savedBooking && savedBooking && savedDevs;
-    }
-
-    public void SortLists()
-    {
-        //PLACE HOLDER
-    }
-    public void SearchLists()
-    {
-        //PLACE HOLDER
-    }
-
 }
--- SaveData.cs
+++ SaveData.cs
@@ -1,55 +1,61 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
 
-public class SaveData
+namespace BookingSystem.SaveData
 {
-    //Sparar till JSON
-    public static bool SaveToFile<T>(List<T> DataToSave, string filePath)
+    public class SaveData
     {
-        try
+        //Sparar till JSON
+        public static bool SaveToFile<T>(List<T> DataToSave, string filePath)
         {
-            //Serialize list till JSON string
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            string jsonString = JsonSerializer.Serialize(DataToSave, options);
+            try
+            {
+                //Serialize list till JSON string
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string jsonString = JsonSerializer.Serialize(DataToSave, options);
 
-            //Skriv JSON string till fil
-            File.WriteAllText(filePath, jsonString);
-            Console.WriteLine("Data sparades korrekt");
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Kunde inte spara data");
-            return false;
-        }
-    }
-
-    //Laddar Från JSON, returnar en lista
-    public static List<T> LoadFromFile<T>(string filePath)
-    {
-        //Kollar om filen finns
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine("Kunde inte hitta filen");
-            return new List<T>();
-        }
-        try
-        {
-            //Läser JSON filen
-            string jsonString = File.ReadAllText(filePath);
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            //Returnar en deserialiserad lista om den lyckas, NULL om den misslyckas
-            List<T>? loadedData = JsonSerializer.Deserialize<List<T>>(jsonString, options);
-            //Null hantering
-            return loadedData ?? new List<T>();
+                //Skriv JSON string till fil
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine("Data sparades korrekt");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Kunde inte spara data");
+                return false;
+            }
         }
 
-
-        catch (Exception e)
+        //Laddar Från JSON, returnar en lista
+        public static List<T> LoadFromFile<T>(string filePath)
         {
-            Console.WriteLine("Kunde inte ladda filen");
-            return new List<T>();
-        }
+            //Kollar om filen finns
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Kunde inte hitta filen");
+                return new List<T>();
+            }
+            try
+            {
+                //Läser JSON filen
+                string jsonString = File.ReadAllText(filePath);
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                //Returnar en deserialiserad lista om den lyckas, NULL om den misslyckas
+                List<T>? loadedData = JsonSerializer.Deserialize<List<T>>(jsonString, options);
+                //Null hantering
+                return loadedData ?? new List<T>();
+            }
 
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Kunde inte ladda filen");
+                return new List<T>();
+            }
+
+        }
     }
 }
 
